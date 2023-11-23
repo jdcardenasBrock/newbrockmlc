e725ed97e862a28b514df2a5ef9356029d17b911
Nicolas Ruiz <nruiz@novacorgroup.com>
2023-11-23 15:35:47 -0500

    our clients vista

---
+++ our_clients.php
@@ -0,0 +1,24 @@
+<?php require 'header.php'; ?>
+<!--==================================================-->
+<!-- Start Euildint Breatcome Area -->
+<!--==================================================-->
+<div class="breatcome-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="breatcome-content">
+                    <div class="breatcome-title">
+                        <h1>Our Clients</h1>
+                    </div>
+                    <div class="bratcome-text">
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                            <li>Our Clients</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require 'footer.php'; ?>
--- restoration.php
+++ restoration.php
@@ -8,12 +8,12 @@
             <div class="col-lg-12 col-md-12">
                 <div class="breatcome-content">
                     <div class="breatcome-title">
-                        <h1>Building</h1>
+                        <h1>Restoration</h1>
                     </div>
                     <div class="bratcome-text">
                         <ul>
                             <li><a href="index.html">Home</a></li>
-                            <li>Building</li>
+                            <li>Restoration</li>
                         </ul>
                     </div>
                 </div>
