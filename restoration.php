d0c029fac7eb43adf3ec534801e12b5c4ae3c604
Nicolas Ruiz <nruiz@novacorgroup.com>
2023-11-23 11:55:50 -0500

    vista de restoration

---
+++ restoration.php
@@ -0,0 +1,83 @@
+<?php require 'header.php'; ?>
+<div class="service-details-area wow animate__slideInUp">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-12">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <!-- start service details single box -->
+                        <div class="service-details-single-box mb-30">
+                            <div class="service-details-thumb">
+                                <img src="assets/images/service/service-details1.jpg" alt="">
+                            </div>
+                            <div class="service-details-content">
+                                <h4><a href="service-details.html">Building smart business solution for you</a></h4>
+                                <p>Lorem ipsum is simply free text used by copytyping refreshing. Neque porro est qui dolorem ipsum a quia quaed inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Aelltes port lacus quis enim var sed sed efficitur turpis gilla sed sit amet eros. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ndustry standard dummy text ever since the 1500s, when an unknown printer took a</p>
+                            </div>
+                        </div>
+                        <!-- End service details single box -->
+                    </div>
+                    <div class="col-lg-12">
+                        <!-- start service details single box -->
+                        <div class="service-details-single-box">
+                            <div class="service-details-thumb">
+                                <img src="assets/images/service/service-details2.jpg" alt="">
+                            </div>
+                            <div class="service-details-content">
+                                <h4><a href="service-details.html">Building Everything Best</a></h4>
+                                <p>Lorem ipsum is simply free text used by copytyping refreshing. Neque porro est qui dolorem ipsum a quia quaed inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Aelltes port lacus quis enim var sed sed efficitur turpis gilla sed sit amet eros. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ndustry standard dummy text ever since the 1500s, when an unknown printer took a</p>
+                            </div>
+                        </div>
+                        <!-- End service details single box -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <!-- start widget search box -->
+                <div class="widget_search box">
+                    <form action="#" method="get">
+                        <input type="text" name="s" value="" placeholder="Search Here" title="Search for:">
+                        <button type="submit" class="icons">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </form>
+                </div>
+                <!-- End widget search box -->
+                <div class="widget-categories-box">
+                    <div class="widget-catagories-title">
+                        <h4>Main Services</h4>
+                    </div>
+                    <!-- widget categories menu -->
+                    <div class="widget-categories-menu">
+                        <ul>
+                            <li><a href="service-details.html"> Business Solution <span><i class="bi bi-chevron-right"></i></span></a></li>	
+                            <li><a href="service-details.html"> Expert Mechanical <span><i class="bi bi-chevron-right"></i></span></a></li>
+                            <li><a href="service-details.html"> Architecture & Building <span><i class="bi bi-chevron-right"></i></span></a></li>
+                            <li><a href="service-details.html"> Construction Management <span><i class="bi bi-chevron-right"></i></span></a></li>
+                            <li><a href="service-details.html"> Apartment Design <span><i class="bi bi-chevron-right"></i></span></a></li>
+                            <li><a href="service-details.html"> Facade Engineering <span><i class="bi bi-chevron-right"></i></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="widget-categories-box">
+                    <!-- categories title -->
+                    <div class="widget-catagories-title">
+                        <h4> Tag Cloud </h4>
+                    </div>
+                    <div class="widget-tags">
+                        <a href="#">Services</a>
+                        <a href="#">Business</a>
+                        <a href="#">Grooming</a>
+                        <a href="#">Breeding</a>
+                        <a href="#">Growth</a>
+                        <a href="#">Stable</a>
+                        <a href="#">Education</a>
+                        <a href="#">America</a>
+                        <a href="#">Maintenance</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require 'footer.php'; ?>
