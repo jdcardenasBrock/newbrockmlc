0327bdc5d646434e397cdfbe5083ee9ae66ae6ea
Juan David Cardenas <110917866+jdcardenasBrock@users.noreply.github.com>
2023-11-23 15:46:53 -0500

    building page created

---
+++ building.php
@@ -0,0 +1,223 @@
+<?php require 'header.php'; ?>
+
+	<!--==================================================-->
+	<!-- Start Euildint About Area -->
+	<!--==================================================-->
+	<div class="about-area inner-page wow animate__slideInUp">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-12">
+					<div class="about-thumb wow animate__slideInUp">
+						<img src="assets/images/about/about.jpg" alt="">
+						<div class="about-shape">
+							<img src="assets/images/about/about-shape.png" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-12">
+					<div class="section-title">
+						<div class="section-shape">
+							<img src="assets/images/about/section-shape.png" alt="">
+						</div>
+						<div class="section-sub-title">
+							<h4>Superior Construction</h4>
+						</div>
+						<div class="section-main-title about">
+							<h2>We Make Finest Architectural</h2>
+							<h2>Design With Great Passion</h2>
+						</div>
+					</div>
+					<div class="about-content wow animate__slideInUp">
+						<div class="about-discribtion">
+							<p>Quisque dignissim enim diam, eget pulvinar ex viverra id. Nulla a lobortis lectus, id volutpat magna. Morbi consequat porttitor fermentum. Nulla vestibulum tincidunt viverra. Vestibulum accumsan</p>
+						</div>
+						<!-- start process bar -->
+						<div class="process-ber-plugin">
+							<span class="process-bar">Customer Satisficed </span>
+							<div id="bar1" class="barfiller">
+								<div class="tipWrap" style="display: inline;">
+									<span class="tip" style="left: 468.814px; transition: left 7s ease-in-out 0s;">78%</span>
+								</div>
+								<span class="fill" data-percentage="78" style="background: rgb(22, 181, 151); width: 505.791px; transition: width 7s ease-in-out 0s;"></span>
+							</div>  
+							<span class="process-bar">Residential Roofing</span>
+							<div id="bar2" class="barfiller">
+								<div class="tipWrap" style="display: inline;">
+									<span class="tip" style="left: 403.254px; transition: left 7s ease-in-out 0s;">55%</span>
+								</div>
+								<span class="fill my-class" data-percentage="55" style="background: rgb(22, 181, 151); width: 438.352px; transition: width 7s ease-in-out 0s;"></span>
+							</div>
+						</div>
+						<!-- End process bar -->
+						<div class="row">
+							<div class="col-lg-6">
+								<!--start about single box -->
+								<div class="about-single-box">
+									<div class="about-icon">
+										<img src="assets/images/about/about-icon1.png" alt="">
+									</div>
+									<div class="about-text">
+										<h4>Worldwide Service</h4>
+										<p>Lorem ipsum is simply velit anod ipas aliquet enean quis.</p>
+									</div>
+								</div>
+								<!--end about single box -->
+							</div>
+							<div class="col-lg-6 col-mdd-6">
+								<!--start about single box -->
+								<div class="about-single-box">
+									<div class="about-icon">
+										<img src="assets/images/about/about-icon2.png" alt="">
+									</div>
+									<div class="about-text">
+										<h4>Awward Winner</h4>
+										<p>Lorem ipsum is simply velit anod ipas aliquet enean quis.</p>
+									</div>
+								</div>
+								<!--end about single box -->
+							</div>
+						</div>
+						<div class="eulding-btn about">
+							<a href="#">Read More <i class="bi bi-arrow-up-right-circle-fill"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--==================================================-->
+	<!-- End Euildint About Area -->
+	<!--==================================================-->
+
+    <!--==================================================-->
+	<!-- Start Euildint Call Do Bg Area -->
+	<!--==================================================-->
+	<div class="call-do-action-area style-two wow animate__slideInUp">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-8 col-md-12">
+					<div class="call-do-title style-two">
+						<h2>Offers Excellentcom Bination</h2>
+						<h2>of Location & Quality</h2>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-12">
+					<div class="single-video style-three text-center">
+						<div class="video-icon style-three">
+							<a class="video-vemo-icon venobox vbox-item" data-vbtype="youtube" data-autoplay="true" href="https://youtu.be/BS4TUd7FJSg"><i class="bi bi-play"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	 </div>
+
+	<!--==================================================-->
+	<!-- End Euildint Call Do Bg Area -->
+	<!--==================================================-->
+
+	<!--==================================================-->
+	<!-- Start Euildint About Area -->
+	<!--==================================================-->
+	<div class="about-area style-two wow animate__slideInUp">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-12">
+					<div class="about-thumb style-two wow animate__slideInUp">
+						<img src="assets/images/about/about2.jpg" alt="">
+						<!-- statr about counter -->
+						<div class="about-counter">
+							<div class="about-numbar">
+								<h4 class="counter">12</h4>
+								<span>+</span>
+							</div>
+							<div class="about-counter-text">
+								<h5>Years of Experience
+									Construction Services</h5>
+							</div>
+						</div>
+						<!-- End about counter -->
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-12">
+					<div class="section-title">
+						<div class="section-shape">
+							<img src="assets/images/about/section-shape.png" alt="">
+						</div>
+						<div class="section-sub-title">
+							<h4>Superior Construction</h4>
+						</div>
+						<div class="section-main-title about">
+							<h2>We Make Finest Architectural</h2>
+							<h2>Design With Great Passion</h2>
+						</div>
+					</div>
+					<div class="about-content wow animate__slideInUp">
+						<div class="about-discribtion">
+							<p>Quisque dignissim enim diam, eget pulvinar ex viverra id. Nulla a lobortis lectus, id volutpat magna. Morbi consequat porttitor fermentum. Nulla vestibulum tincidunt viverra. Vestibulum accumsan</p>
+						</div>
+						<!-- start process bar -->
+						<div class="process-ber-plugin">
+							<span class="process-bar">Customer Satisficed </span>
+							<div id="bar1" class="barfiller">
+								<div class="tipWrap" style="display: inline;">
+									<span class="tip" style="left: 468.814px; transition: left 7s ease-in-out 0s;">78%</span>
+								</div>
+								<span class="fill" data-percentage="78" style="background: rgb(22, 181, 151); width: 505.791px; transition: width 7s ease-in-out 0s;"></span>
+							</div>  
+							<span class="process-bar">Residential Roofing</span>
+							<div id="bar2" class="barfiller">
+								<div class="tipWrap" style="display: inline;">
+									<span class="tip" style="left: 403.254px; transition: left 7s ease-in-out 0s;">55%</span>
+								</div>
+								<span class="fill my-class" data-percentage="55" style="background: rgb(22, 181, 151); width: 438.352px; transition: width 7s ease-in-out 0s;"></span>
+							</div>
+						</div>
+						<!-- End process bar -->
+						<div class="row">
+							<div class="col-lg-6">
+								<!--start about single box -->
+								<div class="about-single-box">
+									<div class="about-icon">
+										<img src="assets/images/about/about-icon1.png" alt="">
+									</div>
+									<div class="about-text">
+										<h4>Worldwide Service</h4>
+										<p>Lorem ipsum is simply velit anod ipas aliquet enean quis.</p>
+									</div>
+								</div>
+								<!--end about single box -->
+							</div>
+							<div class="col-lg-6 col-mdd-6">
+								<!--start about single box -->
+								<div class="about-single-box">
+									<div class="about-icon">
+										<img src="assets/images/about/about-icon2.png" alt="">
+									</div>
+									<div class="about-text">
+										<h4>Awward Winner</h4>
+										<p>Lorem ipsum is simply velit anod ipas aliquet enean quis.</p>
+									</div>
+								</div>
+								<!--end about single box -->
+							</div>
+						</div>
+						<div class="about-bottom">
+							<div class="eulding-btn about">
+								<a href="#">Read More <i class="bi bi-arrow-up-right-circle-fill"></i></a>
+							</div>
+							<div class="about-signiture">
+								<img src="assets/images/about/about-sine.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--==================================================-->
+	<!-- End Euildint About Area -->
+	<!--==================================================-->
+ 
+    
+<?php require 'footer.php'; ?>
