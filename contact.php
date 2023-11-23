7437eb2ced80993163dff4b4333f8f22b2e48cd8
Juan David Cardenas <110917866+jdcardenasBrock@users.noreply.github.com>
2023-11-23 15:24:58 -0500

    ajuste de contact 2

--- contact.php
+++ contact.php
@@ -69,7 +69,7 @@
 			<div class="row contact">
 				<div class="contact-box">
 					<div class="contact-title">
-						<h2>Have you any question?</h2>
+						<h2>Have be any question?</h2>
 						<h2>feel free to Contact</h2>
 					</div>
 					<form action="https://formspree.io/f/myyleorq" method="POST" id="it-form">
@@ -132,7 +132,7 @@
 		<div class="container-fluid p-0">
 			<div class="row">
 				<div class="col-lg-12 col-md-12">
-				<iframe src="https://www.google.com/maps/d/embed?mid=1VzrD0aebBTEuSNlMzEIoqP29E829zCM&ehbc=2E312F" width="640" height="480"></iframe>
+				<iframe src="https://www.google.com/maps/d/embed?mid=1VzrD0aebBTEuSNlMzEIoqP29E829zCM&ehbc=2E312F" width="1920" height="608" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 				</div>
 			</div>
 		</div>
