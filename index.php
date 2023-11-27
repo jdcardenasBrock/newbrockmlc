ac68d1d47a0dc69f5e608471f21a1999bc553bfe
Nicolas Ruiz <nruiz@novacorgroup.com>
2023-11-27 14:46:57 -0500

    video seccion 5 home

--- index.php
+++ index.php
@@ -495,7 +495,13 @@
 				<div class="col-lg-12">
 					<div class="single-video text-center">
 						<div class="video-icon">
-							<a class="video-vemo-icon venobox vbox-item" data-vbtype="youtube" data-autoplay="true" href="https://newbrockmlc.mcardi.com/assets/videos/Commercial%20Building%20Division%20Promo.mp4"><i class="bi bi-play"></i></a>							
+							<a class="video-vemo-icon venobox vbox-item" data-vbtype="youtube" data-autoplay="true" href="https://newbrockmlc.mcardi.com/assets/videos/Commercial%20Building%20Division%20Promo.mp4"><i class="bi bi-play"></i>
+							<video width="320" height="240" controls>
+								<source src="https://newbrockmlc.mcardi.com/assets/videos/Commercial%20Building%20Division%20Promo.mp4" type="video/mp4">
+								<!-- <source src="movie.ogg" type="video/ogg">
+								Your browser does not support the video tag. -->
+							</video>
+						</a>							
 						</div>
 						<div class="call-do-title">
 							<h2>Specialist In Commercial </h2>
