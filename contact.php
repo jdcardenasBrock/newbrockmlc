9f88b1a9998ec9e6b34ab6ce7681570a8d422581
Nicolas Ruiz <nruiz@novacorgroup.com>
2023-11-29 12:27:21 -0500

    ajuste contenido contact us

--- contact.php
+++ contact.php
@@ -74,11 +74,56 @@
 					</div>
 				</div>
 			</div>
+			<div class="row">
+				<div class="col-lg-6 col-md-12">
+					<div class="about-thumb style-two wow animate__slideInUp">
+						<img src="assets/images/building/bui_5.jpg" alt="">
+						<!-- statr about counter -->
+						<div class="about-counter">
+							<div class="about-numbar">
+								<h4 class="counter">25</h4>
+								<span>+</span>
+							</div>
+							<div class="about-counter-text">
+								<h5>Years of Experience</h5>
+							</div>
+						</div>
+						<!-- End about counter -->
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-12">
+					<div class="section-title">
+						<div class="section-shape">
+							<img src="assets/images/resource/section-shape.png" alt="">
+						</div>
+						<div class="section-sub-title">
+							<h4>Contact Us</h4>
+						</div>
+						<div class="section-main-title about">
+							<h2>Contact Information</h2>
+						</div>
+					</div>
+					<div class="about-content wow animate__slideInUp">
+						<div class="about-discribtion">
+							<p>Brock Mountain Land Company, LLC provides clients the very best in excavation and building services. We take pride and are very passionate in the detail of each job. Quality, integrity and expertise enables us to make complex projects simple. Building long term relationships with our clients is key to the success of our business.</p>
+						</div>
+						
+						<!-- <div class="about-bottom-building">
+							<div class="eulding-btn about">
+								<a href="https://www.realtor.com/realestateandhomes-detail/M9419213114">View Our Latest Listing on Realtor.com<i class="bi bi-arrow-up-right-circle-fill"></i></a>
+							</div>
+							<div class="eulding-btn about mt-3">
+								<a href="https://brockmountainhomes.com/">Visit Brock Mountain Homes <i class="bi bi-arrow-up-right-circle-fill"></i></a>
+							</div>
+						</div> -->
+					</div>
+				</div>
+			</div>
 			<div class="row contact">
 				<div class="contact-box">
 					<div class="contact-title">
-						<h2>Have be any question?</h2>
-						<h2>feel free to Contact</h2>
+						<h2>Do you have any question?</h2>
+						<h2>Feel free to contact us</h2>
 					</div>
 					<form action="https://formspree.io/f/myyleorq" method="POST" id="it-form">
 						<div class="row">
