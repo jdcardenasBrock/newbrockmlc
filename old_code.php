09a118d81b8691d86e1284527322c5d6bae0dc42
Nicolas Ruiz <nruiz@novacorgroup.com>
2023-12-18 16:31:19 -0500

    eliminar secciones home

---
+++ old_code.php
@@ -0,0 +1,111 @@
+<!--==================================================-->
+	<!-- Start Euildint Service Area -->
+	<!--==================================================-->
+	<div class="service-area wow animate__slideInUp">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!-- start section title -->
+					<div class="section-title text-center">
+						<div class="section-shape">
+							<img src="assets/images/resource/section-shape.png" alt="">
+						</div>
+						<div class="section-sub-title">
+							<h4>WHAT WE DO</h4>
+						</div>
+						<div class="section-main-title">
+							<h2>High Quality Services Of </h2>
+							<h2>Multiple Industries</h2>
+						</div>
+					</div>
+					<!-- End section title -->
+				</div>
+			</div>
+			<div class="row d-flex justify-content-around">
+				<div class="col-lg-4 col-md-6">
+					<!-- start service single box -->
+					<div class="service-single-box wow animate__slideInUp" id="service-single-box-one">
+						<div class="service-thumb">
+							<img src="assets/images/home/Home_12 whatwedo-2.jpg" alt="">
+						</div>
+						<div class="service-content">
+							<div class="service-icon">
+								<img src="assets/images/resource/service-icon.png" alt="">
+							</div>
+							<div class="service-text">
+								<h4><a href="service-details.html">Restoration</a></h4>
+								<p>Our sitework teams & equipment accessibility combined with our...</p>
+								<a href="restoration.php">Read More <i class="bi bi-arrow-right"></i></a>
+							</div>
+						</div>
+						<div class="service-title" style="right: -51%">
+							<h4>Restoration</h4>
+						</div>
+					</div>
+					<!-- End service single box -->
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<!-- start service single box -->
+					<div class="service-single-box wow animate__slideInUp" id="service-single-box-two">
+						<div class="service-thumb">
+							<img src="assets/images/home/Home_13 whatwedo-2.jpg" alt="">
+						</div>
+						<div class="service-content">
+							<div class="service-icon">
+								<img src="assets/images/resource/service-icon.png" alt="">
+							</div>
+							<div class="service-text">
+								<h4><a href="service-details.html">Building</a></h4>
+								<p>Our premier project management solutions are trusted by industrial...</p>
+								<a href="building.php">Read More <i class="bi bi-arrow-right"></i></a>
+							</div>
+						</div>
+						<div class="service-title" style="right: -47%">
+							<h4>Building</h4>
+						</div>
+					</div>
+					<!-- End service single box -->
+				</div>				
+			</div>
+		</div>
+	</div>
+	<!--==================================================-->
+	<!-- End Euildint Service Area -->
+	<!--==================================================-->
+
+	<!--==================================================-->
+	<!-- Start Euildint Contact Area -->
+	<!--==================================================-->
+	<div class="contact-area wow animate__slideInUp">
+		<!-- <div class="container"> -->
+			<div class="row align-items-center">
+				<div class="col-lg-8 col-md-12" style="padding-left: 15%">
+					<div class="container">
+						<div class="section-title">
+							<div class="section-main-title contact">
+								<h2>Contact us Today And Let us</h2>
+								<h2>Know What you Need</h2>
+							</div>
+						</div>
+						<div class="contact-discription">
+							<p>Brock Mountain Land Company, LLC provides clients the very best in excavation and building services. We take pride and are very passionate in the detail of each job...</p>
+						</div>
+						<div class="eulding-btn">
+							<a href="contact.php">Contact Us <i class="bi bi-arrow-up-right-circle-fill"></i></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="contact-thumb wow animate__slideInRight">
+						<img src="assets/images/home/Home_15-contact copia.png" alt="">
+						<!-- <div class="contact-shape">
+							<img src="assets/images/resource/contact-shape.png" alt="">
+						</div> -->
+					</div>
+				</div>
+			</div>
+		<!-- </div> -->
+	</div>
+	<!--==================================================-->
+	<!-- End Euildint Contact Area -->
+	<!--==================================================-->
